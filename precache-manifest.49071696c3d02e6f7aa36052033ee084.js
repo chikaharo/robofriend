6e5d451f10a350e876be2ffacffbd18ee9a1fa37
chikaharo <madhattervn@gmail.com>
2020-05-16 17:11:23 +0700

    Updates

---
+++ precache-manifest.49071696c3d02e6f7aa36052033ee084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433f234773161610d5751eba8d485787",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c92082afaf05216c1103",
+    "url": "/robofriend/static/css/main.cc8b76e0.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriend/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92082afaf05216c1103",
+    "url": "/robofriend/static/js/main.44d176b5.chunk.js"
+  },
+  {
+    "revision": "304188c1743133897544",
+    "url": "/robofriend/static/js/runtime-main.8ce40bb9.js"
+  }
+]);
